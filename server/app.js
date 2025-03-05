3d4cae5547fc393648028b98d2c37fd8cfff1e2d
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-03-05 12:11:28 +0100

    add: .env

---
+++ server/app.js
@@ -0,0 +1,23 @@
+require ("dotenv").config(); //serve per caricare il file .env 
+const express = require ('express');
+const app = express();
+
+const cors = require ('cors');
+const port = 3000;
+
+
+app.use(cors({
+origin: 'http://localhost:5173'
+}));
+
+
+app.use(express.static('public'));
+
+app.use(express.json());
+
+
+
+
+app.listen(port, () => {
+    console.log("server in ascolto sulla porta" + port);
+})
